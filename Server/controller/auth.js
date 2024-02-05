bc0b7c0610f8f4fff35f078273751f0f8020fbcd
Raja28 <rajadavid03@gmail.com>
2024-02-05 21:02:21 +0530

    add du-1

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -2,3 +2,5 @@
 function auth(){
     console.log("testing git from auth");
 }
+
+// dummy update - 1
--- Server/models/User.js
+++ Server/models/User.js
@@ -2,3 +2,5 @@
 function model(){
     console.log("testing push on git from model");
 }
+
+//dummy update - 1
